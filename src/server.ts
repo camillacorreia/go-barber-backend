d025d91182d2bee14c13128e804a7927148a3371
Camilla Correia <camilla.correia@cubos.io>
2020-09-01 21:12:58 -0300

    feat: endpoint de cadastro de agendamento

--- src/server.ts
+++ src/server.ts
@@ -6,17 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email
-  };
-
-  return response.json(user);
-});
-
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
 })
