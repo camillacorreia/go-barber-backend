f675dc8a187c9c235354f589d8c7f9ac969f9c1b
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-09-16 20:23:52 -0300

    refactor: alterações no repositório do TypeORM

--- src/database/migrations/1600295574237-CreateAppointments.ts
+++ src/database/migrations/1600295574237-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1600295574237 implements MigrationInterfa
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
