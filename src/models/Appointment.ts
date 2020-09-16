09dd82520d77fdedf9914c849a9e075c544cadf4
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-09-16 19:56:47 -0300

    feat: criação do model de agendamentos

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,17 +1,15 @@
-import { v4 } from 'uuid';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({provider, date}: Omit<Appointment, 'id'>) {
-    this.id = v4();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
