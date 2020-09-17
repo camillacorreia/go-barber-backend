4bd24977aab613d726f05b0c4a2374ec9e34adce
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-09-16 23:23:50 -0300

    feat: relacionamento nos models

--- src/database/migrations/1600295574237-CreateAppointments.ts
+++ src/database/migrations/1600295574237-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1600295574237 implements MigrationInterfa
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,6 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
@@ -6,10 +8,14 @@ class Appointment {
   id: string;
 
   @Column()
-  provider: string;
+  provider_id: string;
 
   @Column('timestamp with time zone')
   date: Date;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id'})
+  provider: User;
 
   @CreateDateColumn()
   created_at: Date;
