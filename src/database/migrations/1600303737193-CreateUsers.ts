d1e472d5b7f343eb3ed71398109f92590ac6ae85
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-09-16 22:39:27 -0300

    feat: criação do model e migration de usuários
    Foram realizadas algumas alterações na tabela de agendamentos.

--- src/database/migrations/1600303737193-CreateUsers.ts
+++ src/database/migrations/1600303737193-CreateUsers.ts
@@ -1,11 +1,11 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import {MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export default class CreateAppointments1600295574237 implements MigrationInterface {
+export default class CreateUsers1600303737193 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,14 +15,17 @@ export default class CreateAppointments1600295574237 implements MigrationInterfa
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
-            isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'epassword',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -40,7 +43,7 @@ export default class CreateAppointments1600295574237 implements MigrationInterfa
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 
 }
