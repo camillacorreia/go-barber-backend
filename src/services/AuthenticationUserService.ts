1078311ebc003169c179e59bfad0a209a6307f36
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-09-22 19:34:28 -0300

    feat: gerando token JWT

--- src/services/AuthenticationUserService.ts
+++ src/services/AuthenticationUserService.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +11,7 @@ interface Request {
 
 interface Response {
     user: User;
+    token: string;
   }
 
 class AuthenticationUserService {
@@ -27,8 +30,14 @@ class AuthenticationUserService {
         throw new Error('Email ou senha incorreta');
     }
 
+    const token = sign({}, 'b38449bd30654267346a5f921dd6d6c3', {
+        subject: user.id,
+        expiresIn: '1d',
+    });
+
     return {
         user,
+        token,
     }
   }
 }
