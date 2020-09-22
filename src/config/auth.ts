1c6110e679374de2e98feacc6d14281e79757630
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-09-22 20:44:22 -0300

    feat: rotas autenticadas
    fix: removi erro ao adicionar novos tipos para Request com @types/passport

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'b38449bd30654267346a5f921dd6d6c3',
+        expiresIn: '1d',
+    },
+};
--- src/services/AuthenticationUserService.ts
+++ src/services/AuthenticationUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -15,29 +16,31 @@ interface Response {
   }
 
 class AuthenticationUserService {
-  public async execute({ email, password} : Request): Promise<Response> {
+  public async execute({ email, password } : Request): Promise<Response> {
     const usersRepository = getRepository(User);
 
     const user = await usersRepository.findOne({ where: { email }});
 
     if (!user) {
-        throw new Error('Email ou senha incorreta');
+      throw new Error('Email ou senha incorreta');
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-        throw new Error('Email ou senha incorreta');
+      throw new Error('Email ou senha incorreta');
     }
 
-    const token = sign({}, 'b38449bd30654267346a5f921dd6d6c3', {
-        subject: user.id,
-        expiresIn: '1d',
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
     });
 
     return {
-        user,
-        token,
+      user,
+      token,
     }
   }
 }
